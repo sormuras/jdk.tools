e451891c20f783e17f7610066058cdd2426bfd1e
Christian Stein <sormuras@gmail.com>
2023-04-16 07:23:14 +0200

    Improve module documentation

--- module-info.java
+++ module-info.java
@@ -25,50 +25,60 @@
 
 /**
  * Defines the Java Tooling API.
- * <p>
- * The basic building block of the Java Tooling API is {@link java.util.spi.ToolProvider} defined in
- * the {@code java.base} module. This ensures that all existing and future tools implementing that
- * base interface are supported by the Java Tooling API.
- * <p>
- * The {@link jdk.tools.Tool} interface of this module describes a {@link java.util.spi.ToolProvider
- * ToolProvider} instance and binds it to an optional namespace and non-empty name. By default, the
- * namespace and name are computed by inspecting provider instance properties. For example:
- * {@snippet
- *   jdk.tools.Tool jar = jdk.tools.Tool.of("jar");
+ *
+ * <p>The basic building block of the Java Tooling API is the {@link java.util.spi.ToolProvider
+ * ToolProvider} interface defined by module {@code java.base} in the {@code java.util.spi} package.
+ * This ensures that all existing and future tools implementing that base interface are supported by
+ * the Java Tooling API.
+ *
+ * <p>In module {@code jdk.tools}, the {@link jdk.tools.Tool Tool} interface describes a {@link
+ * java.util.spi.ToolProvider ToolProvider} instance and binds it to an optional namespace and
+ * non-empty name. By default, the namespace and name are computed by inspecting provider instance
+ * properties. For example:
+ *
+ * <pre>{@snippet :
+ *   var jar = Tool.of("jar");
+ *
  *   assert jar.namespace().equals("jdk.jartool");
  *   assert jar.name().equals("jar");
  *   assert jar.provider() instanceof java.util.spi.ToolProvider;
- * }
- * <p>
- * An implementation of the {@link jdk.tools.ToolFinder} interface represents a search-able sequence
- * of tool instances. A tool can be searched for by passing its name to the
- * {@link jdk.tools.ToolFinder#find(String)} method. Use the list returned by the
- * {@link jdk.tools.ToolFinder#tools()} accessor to perform custom searches.
+ * }</pre>
  *
- * Note that the {@link jdk.tools.Tool} interface
- * extends the {@link jdk.tools.ToolFinder} interface (containing itself as the only tool instance)
- * in order to allow better composibility via the {@link jdk.tools.ToolFinder#compose(jdk.tools.ToolFinder...)}
- * factory method. Here are examples of commonly used factory methods:
+ * <p>An implementation of the {@link jdk.tools.ToolFinder ToolFinder} interface represents a
+ * search-able sequence of tool instances. A tool can be searched for by passing its name to the
+ * {@link jdk.tools.ToolFinder#find(String)} method. Use the list returned by the {@link
+ * jdk.tools.ToolFinder#tools()} accessor to perform custom searches.
+ *
+ * <p>Note that the {@link jdk.tools.Tool Tool} interface extends the {@link jdk.tools.ToolFinder
+ * ToolFinder} interface (containing itself as the only tool instance) in order to allow better
+ * composability via the {@link jdk.tools.ToolFinder#compose(jdk.tools.ToolFinder...)} factory
+ * method. Here are examples of commonly used factory methods:
+ *
  * <ul>
- * <li>{@link jdk.tools.ToolFinder#of(String...)} ...
- * <li>{@link jdk.tools.ToolFinder#of(jdk.tools.Tool...)} ...
- * <li>{@link jdk.tools.ToolFinder#of(ModuleLayer)} ...
+ *   <li>{@link jdk.tools.ToolFinder#of(String...)} ...
+ *   <li>{@link jdk.tools.ToolFinder#of(jdk.tools.Tool...)} ...
+ *   <li>{@link jdk.tools.ToolFinder#of(ModuleLayer)} ...
  * </ul>
- * <p>
- * An implementation of the {@link jdk.tools.ToolRunner} interface offers various methods to run
- * instances of {@link jdk.tools.Tool}. It also provides run-time relevant components and constants.
- * Here is an exemplary usage pattern:
- * {@snippet
+ *
+ * <p>An implementation of the {@link jdk.tools.ToolRunner ToolRunner} interface offers various
+ * methods to execute instances of {@link jdk.tools.Tool Tool} using the linked {@link
+ * java.util.spi.ToolProvider ToolProvider} object. It also provides run-time relevant components
+ * and constants. Here is an exemplary usage pattern:
+ *
+ * <pre>{@snippet :
  *   var finder = ToolFinder.of("jar", "javac");
  *   var runner = ToolRunner.of(finder);
+ *
  *   runner.run("jar", "--version");
  *   runner.run("javac", "--version");
- * }
- * <p>
- * The {@link jdk.tools.ToolOperator} interface is a {@link java.util.spi.ToolProvider} extension
- * implemented by classes want to be capable of running other tools. For example:
- * {@snippet
- * public record VersionPrinter(String name) implements jdk.tools.ToolOperator {
+ * }</pre>
+ *
+ * <p>The {@link jdk.tools.ToolOperator ToolOperator} interface is a {@link
+ * java.util.spi.ToolProvider ToolProvider} extension implemented by classes that want to be capable
+ * of running other tools. For example:
+ *
+ * <pre>{@snippet :
+ * public record VersionPrinter(String name) implements ToolOperator {
  *   public VersionPrinter() { this("versions"); }
  *   public int run(ToolRunner runner, PrintWriter out, PrintWriter err, String... names) {
  *     if (names.length == 0) {
@@ -80,19 +90,32 @@
  *     return 0;
  *   }
  * }
- * <p>
- * {@link jdk.tools.Command} represents a composable tool call from the command-line.
- * For example, the tool call {@code javac --version} can be composed via:
+ * }</pre>
+ *
+ * <p>{@link jdk.tools.Command Command} represents a tool call from the command-line that is
+ * programmatically composable. For example, the tool call {@code javac --version} can be composed
+ * via:
+ *
  * <ul>
  *   <li>{@code Command.ofCommandLine("javac --version")}
  *   <li>{@code Command.ofCommand(List.of("javac", "--version")}
  *   <li>{@code Command.of("javac").with("--version")}
  *   <li>{@code Command.of("javac", "--version")}
  * </ul>
- * Task -  tool operator extension for running an ordered collection of command instances.
- * <p>
- * The {@link jdk.tools.ToolMenu} interface supports creating subcommand hierarchies using nested
- * {@link jdk.tools.ToolFinder} instances.
+ *
+ * <p>The {@link jdk.tools.Task Task} interface is tool operator extension for running an ordered
+ * collection of command instances.
+ *
+ * <pre> {@snippet :
+ *   var versions = Task.of("preset", "versions", Command.of("jar", "--version"), Command.of("javac", "--version"));
+ *   var finder = ToolFinder.compose(ToolFinder.of("jar", "javac"), versions);
+ *   var runner = ToolRunner.of(finder);
+ *
+ *   runner.run("versions");
+ * } </pre>
+ *
+ * <p>The {@link jdk.tools.ToolMenu ToolMenu} interface supports creating subcommand hierarchies
+ * using nested {@link jdk.tools.ToolFinder ToolFinder} instances.
  */
 module jdk.tools {
   requires jdk.jfr;
